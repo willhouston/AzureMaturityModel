82a5cead612aaab627f959a6c5d778f14ba20c69
Will Houston <william.e.houston@ehi.com>
2020-09-14 07:48:48 +0000

    Added SolutionMaturityView

---
+++ AzureMaturityModel.Database/SolutionMaturityView.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[SolutionMaturityView] AS
+	SELECT s.SolutionName, m.MaturityMetricName, ml.MaturityLevelName
+        FROM    
+            Solution as s
+                INNER JOIN SolutionMetric sm
+                    ON s.SolutionId = sm.SolutionId
+                INNER JOIN MaturityLevel ml
+                    ON sm.MaturityLevelId = ml.MaturityLevelId
+                INNER JOIN MaturityMetric m
+                    ON sm.MaturityMetricId = m.MaturityMetricId
