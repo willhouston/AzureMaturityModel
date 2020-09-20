3cec69e65cff6f775ec59cab69398c549aa3e5fa
willhouston <will@willhouston.co.uk>
2020-09-20 16:51:00 +0100

    Adding migration script

---
+++ AzureMaturityModel.Api/Migrations/20200920151348_Initial creation.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AzureMaturityModel.Api.Migrations
+{
+    public partial class Initialcreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MaturityLevel",
+                columns: table => new
+                {
+                    MetricCategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MaturityLevelName = table.Column<string>(maxLength: 50, nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaturityLevel", x => x.MetricCategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MetricCategory",
+                columns: table => new
+                {
+                    MetricCategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MetricCategoryName = table.Column<string>(maxLength: 50, nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MetricCategory", x => x.MetricCategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Organisation",
+                columns: table => new
+                {
+                    OrganisationId = table.Column<Guid>(nullable: false),
+                    OrganisationName = table.Column<string>(maxLength: 50, nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organisation", x => x.OrganisationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MaturityMetric",
+                columns: table => new
+                {
+                    MaturityMetricId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MaturityMetricName = table.Column<string>(maxLength: 50, nullable: false),
+                    Description = table.Column<string>(maxLength: 1000, nullable: true),
+                    SortKey = table.Column<int>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: false),
+                    MetricCategoryId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaturityMetric", x => x.MaturityMetricId);
+                    table.ForeignKey(
+                        name: "FK_MaturityMetric_MetricCategory_MetricCategoryId",
+                        column: x => x.MetricCategoryId,
+                        principalTable: "MetricCategory",
+                        principalColumn: "MetricCategoryId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Team",
+                columns: table => new
+                {
+                    TeamId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeamName = table.Column<string>(maxLength: 50, nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: false),
+                    OrganisationId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Team", x => x.TeamId);
+                    table.ForeignKey(
+                        name: "FK_Team_Organisation_OrganisationId",
+                        column: x => x.OrganisationId,
+                        principalTable: "Organisation",
+                        principalColumn: "OrganisationId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Solution",
+                columns: table => new
+                {
+                    SolutionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SolutionName = table.Column<string>(maxLength: 50, nullable: false),
+                    InternalRef = table.Column<string>(maxLength: 50, nullable: true),
+                    Description = table.Column<string>(maxLength: 1000, nullable: true),
+                    StatusId = table.Column<int>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: false),
+                    TeamId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Solution", x => x.SolutionId);
+                    table.ForeignKey(
+                        name: "FK_Solution_Team_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Team",
+                        principalColumn: "TeamId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SolutionMetric",
+                columns: table => new
+                {
+                    SolutionId = table.Column<int>(nullable: false),
+                    MaturityMetricId = table.Column<int>(nullable: false),
+                    EnvironmentTag = table.Column<string>(maxLength: 50, nullable: true),
+                    MaturityLevelMetricCategoryId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SolutionMetric", x => new { x.SolutionId, x.MaturityMetricId });
+                    table.ForeignKey(
+                        name: "FK_SolutionMetric_MaturityLevel_MaturityLevelMetricCategoryId",
+                        column: x => x.MaturityLevelMetricCategoryId,
+                        principalTable: "MaturityLevel",
+                        principalColumn: "MetricCategoryId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SolutionMetric_MaturityMetric_MaturityMetricId",
+                        column: x => x.MaturityMetricId,
+                        principalTable: "MaturityMetric",
+                        principalColumn: "MaturityMetricId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SolutionMetric_Solution_SolutionId",
+                        column: x => x.SolutionId,
+                        principalTable: "Solution",
+                        principalColumn: "SolutionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaturityMetric_MetricCategoryId",
+                table: "MaturityMetric",
+                column: "MetricCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Solution_TeamId",
+                table: "Solution",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SolutionMetric_MaturityLevelMetricCategoryId",
+                table: "SolutionMetric",
+                column: "MaturityLevelMetricCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SolutionMetric_MaturityMetricId",
+                table: "SolutionMetric",
+                column: "MaturityMetricId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Team_OrganisationId",
+                table: "Team",
+                column: "OrganisationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SolutionMetric");
+
+            migrationBuilder.DropTable(
+                name: "MaturityLevel");
+
+            migrationBuilder.DropTable(
+                name: "MaturityMetric");
+
+            migrationBuilder.DropTable(
+                name: "Solution");
+
+            migrationBuilder.DropTable(
+                name: "MetricCategory");
+
+            migrationBuilder.DropTable(
+                name: "Team");
+
+            migrationBuilder.DropTable(
+                name: "Organisation");
+        }
+    }
+}
